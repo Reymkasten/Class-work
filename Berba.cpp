15185075fdb40c817a487bdfbbd49185c2aee4c0
Reymkasten <163716776+Reymkasten@users.noreply.github.com>
2024-04-07 10:11:32 +0200

    Add files via upload

---
+++ Berba.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+class Book {//сподіваюсь ви пам`ятаєте про душу (весь текст англійською бо у мене візуал не підтримує українську, лише в коментарях)
+private:
+    std::string author;
+    std::string title;
+    int year;
+    int pageCount;
+
+public:
+    
+    Book(const std::string& _author, const std::string& _title, int _year, int _pageCount)
+        : author(_author), title(_title), year(_year), pageCount(_pageCount) {}
+
+    
+    Book(const Book& other)
+        : author(other.author), title(other.title), year(other.year), pageCount(other.pageCount) {}
+
+   
+    void print() const {
+        std::cout << "Author: " << author << std::endl;
+        std::cout << "Name: " << title << std::endl;
+        std::cout << "Year: " << year << std::endl;
+        std::cout << "Page amount: " << pageCount << std::endl;
+    }
+};
+
+int main() {
+    Book book1("Bebra Bebrivna", "2077", 2077, 69);
+    Book book2 = book1;
+
+    std::cout << " Info 1:" << std::endl;
+    book1.print();
+    std::cout << std::endl;
+
+    std::cout << " Info 2 (copy info 1):" << std::endl;
+    book2.print();
+
+    return 0;
+}
---
+++ classes.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+class Date {   //Продайте свою душу щоб далі читати
+private:
+    int day;
+    int month;
+    int year;
+
+public:
+    
+    Date(int _day, int _month, int _year) : day(_day), month(_month), year(_year) {}
+
+   
+    Date(const Date& other) : day(other.day), month(other.month), year(other.year) {}
+
+    
+    Date getNewer(const Date& other) const {
+        if (year > other.year)
+            return *this;
+        else if (year < other.year)
+            return other;
+        else {
+            if (month > other.month)
+                return *this;
+            else if (month < other.month)
+                return other;
+            else {
+                if (day > other.day)
+                    return *this;
+                else if (day < other.day)
+                    return other;
+                else
+                    std::cout << "it is equal" << std::endl;
+            }
+        }
+    }
+
+    
+    void print() const {
+        std::cout << day << "." << month << "." << year << std::endl;;
+    }
+};
+
+int main() {
+    
+    Date firstDate(7, 4, 2024);
+    Date secondDate(15, 8, 2023);
+
+    std::cout << "First date: ";
+    firstDate.print();
+    std::cout << std::endl;
+
+    std::cout << "Second date: ";
+    secondDate.print();
+    std::cout << std::endl;
+
+    Date newerDate = firstDate.getNewer(secondDate);
+
+    std::cout << "New date: ";
+    newerDate.print();
+    std::cout << std::endl;
+
+    return 0;
+}
